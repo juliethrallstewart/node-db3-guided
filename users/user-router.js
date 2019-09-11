3964595e38339b9c192d3604801772c2a32a7659
Julie Thrall <juliethrallstewart@gmail.com>
2019-09-11 10:44:23 -0700

    fixed missing id

--- users/user-router.js
+++ users/user-router.js
@@ -33,6 +33,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.get('/:id/posts', (req, res) => {
+  const {id} = req.params
   db('posts as p')
     .join('users as u', 'u.id', '=', 'p.user_id')
     .where({user_id: id})
